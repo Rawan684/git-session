06c2d3f95e2005165c28870b5fed96385f76096f
rawan <rawanesmandar0@gmail.com>
2024-11-02 19:24:09 +0300

    add new controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
 
-    public function login() {}
+    public function login(Request $request)
+    {
+        $request->validated($request->all());
+    }
 }
